fe8c901bb1850b94c51f43b8aab598d0a06cb9cb
Tharun <tharunyn@gmail.com>
2025-10-24 21:23:45 +0530

    change

--- componentts/CodeEditor.tsx
+++ componentts/CodeEditor.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import Editor from "@monaco-editor/react";
 
 export default function CodeEditor() {
-  const [code, setCode] = useState("// You can write ur code guyzzz");
+  const [code, setCode] = useState("// You can write ur code guyz");
   const [output, setOutput] = useState("");
 
   // Function to run the code
