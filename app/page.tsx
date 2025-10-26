8a379b67be209662d4c449f536bf23c77d0997f8
Tharun <tharunyn@gmail.com>
2025-10-26 19:54:58 +0530

    change

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,6 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-// Dynamically import the editor so it only loads in the browser
-const CodeEditor = dynamic(() => import("../components/CodeEditor"), {
-  ssr: false, // Disable server-side rendering for this component
-});
+import CodeEditor from "../components/CodeEditor";
 
 export default function Home() {
-  return (
-    <main className="h-screen w-full">
-      <CodeEditor />
-    </main>
-  );
+  return <CodeEditor />;
 }
+
